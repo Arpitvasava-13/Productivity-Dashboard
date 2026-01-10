879027ee69ca62f0f0080ac2e9f3879026be3aa6
Arpit Vasava <av4978033@gmail.com>
2026-01-10 12:09:48 +0530

    DAY 2

--- script.js
+++ script.js
@@ -1,8 +1,73 @@
-var allElems = document.querySelectorAll(".elem");
-var allFullElems = document.querySelectorAll(".fullElem");
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var FullElemPage = document.querySelectorAll(".fullElem");
+  var FullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    allFullElems[elem.id].style.display = "block";
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      FullElemPage[elem.id].style.display = "block";
+    });
   });
-});
+
+  FullElemPageBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      FullElemPage[back.id].style.display = "none";
+    });
+  });
+}
+openFeatures();
+
+function todoList() {
+  var currentTask = [];
+
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("empty");
+  }
+
+  function renderTask() {
+    var allTask = document.querySelector(".allTask");
+
+    var sum = "";
+
+    currentTask.forEach(function (elem, idx) {
+      sum =
+        sum +
+        `<div class="task">
+              <h5>${elem.task}</h5>
+              <span class=${elem.imp}>imp</span>
+              <button id=${elem.idx}>Mark As Completed</button>
+            </div>`;
+    });
+    allTask.innerHTML = sum;
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+
+    document.querySelectorAll(".task button").forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        currentTask.splice(btn.id, 1);
+        renderTask();
+      });
+    });
+  }
+  renderTask();
+
+  let form = document.querySelector(".addTask form");
+  let taskInput = document.querySelector(".addTask form #task-input");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let taskCheckbox = document.querySelector(".addTask form #check");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckbox.checked,
+    });
+    renderTask();
+    taskCheckbox.checked = false;
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+  });
+}
+todoList();
